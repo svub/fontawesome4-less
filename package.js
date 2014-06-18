925e820b149b72a94aafc57585acdd1fd2165c61
svub <me@svenbuschbeck.net>
2014-06-18 21:43:18 +0300

    Update to 4.1 (inspired by https://github.com/xdissent); minor improvement in #fa-icon;

--- package.js
+++ package.js
@@ -1,9 +1,13 @@
 Package.describe({
-    summary: "Font Awesome 4's LESS sources. Using this package will allow you reuse mixins and variables. As default, import '/packages/font-awesome-4-less/font-awesome.lessimport'"
+  summary: "Font Awesome 4's LESS sources. Using this package will allow you reuse mixins and variables. As default, import '/packages/font-awesome-4-less/font-awesome.lessimport'"
 });
 
 Package.on_use(function(api){
-	api.use('less','client');
+  api.use('less','client');
+  api.add_files('lib/fonts/fontawesome-webfont.eot', 'client');
+  api.add_files('lib/fonts/fontawesome-webfont.svg', 'client');
+  api.add_files('lib/fonts/fontawesome-webfont.ttf', 'client');
+  api.add_files('lib/fonts/fontawesome-webfont.woff', 'client');
 });
 
 Package.on_test(function(api){
